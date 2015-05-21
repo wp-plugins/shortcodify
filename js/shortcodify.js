0bbff0fbe164d86df1681b827e66d33ef9c11f37
lrnz <lrnz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-21 20:09:44 +0000

    [FEATURE] Version V 1.4.0 with two new shortcodes
    git-svn-id: https://plugins.svn.wordpress.org/shortcodify/tags/1.4.0@1165218 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/shortcodify.js
@@ -0,0 +1,11 @@
+jQuery(function() {
+	jQuery( ".sc_accordion.autoheight" ).accordion();
+    jQuery( ".sc_accordion.collapsible" ).accordion({
+	    collapsible: true
+    });
+    jQuery( ".sc_accordion.no-auto" ).accordion({
+	    heightStyle: "content"
+    });
+    
+    jQuery( ".sc_tabs.std" ).tabs();
+});
